735469261006ccb9c4ac2764a4e17a02e8304891
lingen <*\(^_^)/*>
2022-05-17 14:58:29 +0800

    aviator使用

---
+++ aviator_demo/src/main/java/Main3.java
@@ -0,0 +1,18 @@
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
+public class Main3 {
+  public static void main(String[] args)
+      throws ScriptException, IllegalAccessException, NoSuchMethodException {
+    // js引擎和反射执行的demo
+
+    ScriptEngineManager manager = new ScriptEngineManager();
+    ScriptEngine engine = manager.getEngineByName("AviatorScript");
+    engine.put("a", 2);
+    engine.put("b", 5);
+    Object eval = engine.eval("[1,2].includes(a)");
+
+    System.out.println(eval);
+  }
+}
---
+++ aviator_demo/src/main/java/StringUtils.java
@@ -0,0 +1,9 @@
+public class StringUtils {
+
+  public static boolean equals(Object a, Object b) {
+
+    boolean result = true;
+
+    return result;
+  }
+}
