af8f904f418885c25c5b7dba8b7f5ea3e643b366
lg <1762861715@qq.com>
2023-10-16 09:26:12 +0800

    表达式用法

--- aviator_demo/src/test/java/MainTest.java
+++ aviator_demo/src/test/java/MainTest.java
@@ -11,17 +11,20 @@ public class MainTest {
     public void test1() {
         // 表达式参数
         HashMap<String, Object> map = new HashMap<>();
-        map.put("a", 1);
-        map.put("b", 2);
-        map.put("c", 3);
+        map.put("a" , 1);
+        map.put("b" , 2);
+        map.put("c" , 3);
 
         // 2.表达式校验
         String expression = "a-(b-c) > 100";
+        expression = "1+2>1 || 1-21>1";
+        expression = "1+2>1&&1+21>1";
+        expression = "\"a\"==a";
+        Integer a = 1;
+
         try {
             AviatorEvaluator.validate(expression);
             Expression compiledExp = AviatorEvaluator.compile(expression);
-            // Execute with injected variables.
-
             Boolean data = (Boolean) compiledExp.execute(map);
             System.out.println(data);
         } catch (Exception e) {
