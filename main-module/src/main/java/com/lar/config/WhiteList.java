cab24e9d8176a9e1194d928b8a6bff3cb38910a4
lg <1762861715@qq.com>
2024-03-08 14:56:45 +0800

    简化

--- main-module/src/main/java/com/lar/config/WhiteList.java
+++
@@ -1,9 +0,0 @@
-package com.lar.config;
-
-import java.util.ArrayList;
-
-public class WhiteList {
-    public static final ArrayList<String> url = new ArrayList<>() {{
-        add("/user/login");
-    }};
-}
--- main-module/src/main/java/com/lar/test/spi/Phone.java
+++
@@ -1,6 +0,0 @@
-package com.lar.test.spi;
-
-public interface Phone {
-    String getName();
-
-}
